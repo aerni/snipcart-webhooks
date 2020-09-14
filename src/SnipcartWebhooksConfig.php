558212fedcc4be8ad52ef3951993b04b919cff6a
Michael Aerni <hello@michaelaerni.ch>
2020-09-14 13:52:48 +0200

    Move apiSecret method to own config class

---
+++ src/SnipcartWebhooksConfig.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Aerni\SnipcartWebhooks;
+
+use Aerni\SnipcartWebhooks\Exceptions\ApiSecretNotFoundException;
+
+class SnipcartWebhooksConfig
+{
+    /**
+     * Get the secret Snipcart API Key by mode.
+     *
+     * @return string
+     */
+    public function apiSecret(): string
+    {
+        $mode = config('snipcart-webhooks.test_mode');
+
+        $apiSecret = $mode
+            ? config('snipcart-webhooks.test_secret')
+            : config('snipcart-webhooks.live_secret');
+
+        if (! $apiSecret) {
+            throw new ApiSecretNotFoundException($mode);
+        }
+
+        return $apiSecret;
+    }
+}
--- src/SnipcartWebhooksServiceProvider.php
+++ src/SnipcartWebhooksServiceProvider.php
@@ -2,23 +2,22 @@
 
 namespace Aerni\SnipcartWebhooks;
 
-use Aerni\SnipcartWebhooks\Exceptions\ApiSecretNotFoundException;
 use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class SnipcartWebhooksServiceProvider extends ServiceProvider
 {
-    public function register()
+    public function register(): void
     {
         $this->app->singleton(SignatureValidator::class, function () {
-            return new SignatureValidator($this->apiSecret());
+            return new SignatureValidator((new SnipcartWebhooksConfig())->apiSecret());
         });
 
         $this->mergeConfigFrom(__DIR__.'/../config/snipcart-webhooks.php', 'snipcart-webhooks');
     }
 
-    public function boot()
+    public function boot(): void
     {
         Route::macro('snipcart', function (string $url, string $name = 'default') {
             Route::post($url, '\Aerni\SnipcartWebhooks\SnipcartWebhooksController')->name("snipcart-webhooks-{$name}")->withoutMiddleware([VerifyCsrfToken::class]);
@@ -27,25 +26,5 @@ class SnipcartWebhooksServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/../config/snipcart-webhooks.php' => config_path('snipcart-webhooks.php'),
         ], 'config');
-    }
-
-    /**
-     * Returns the secret Snipcart API Key.
-     *
-     * @return string
-     */
-    protected function apiSecret()
-    {
-        $mode = config('snipcart-webhooks.test_mode');
-
-        $apiSecret = $mode
-            ? config('snipcart-webhooks.test_secret')
-            : config('snipcart-webhooks.live_secret');
-
-        if (! $apiSecret) {
-            throw new ApiSecretNotFoundException($mode);
-        }
-
-        return $apiSecret;
     }
 }
